1efd6f52b65fb17acbe49de17b51ec32b6e5028e
ProstoCheI <shishkinm17@gmail.com>
2024-07-11 16:06:10 +0500

    Add new file

---
+++ Game_num.py
@@ -0,0 +1,10 @@
+from random import randint
+num_p1 = int(input())
+num_p2 = int(input())
+num = randint(1, 6)
+if num_p1 == num:
+    print('Победил первый игрок')
+elif num_p2 == num:
+    print('Победил второй игрок')
+else:
+    print('Ничья')
