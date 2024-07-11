a7dc4fcb955d6a03d1f9ef122d966d115c808c1d
ProstoCheI <shishkinm17@gmail.com>
2024-07-11 16:08:02 +0500

    Added comments in the input lines.

--- Game_num.py
+++ Game_num.py
@@ -1,6 +1,6 @@
 from random import randint
-num_p1 = int(input())
-num_p2 = int(input())
+num_p1 = int(input('Введите число первого игрока:'))
+num_p2 = int(input('Введите число второго игрока: '))
 num = randint(1, 6)
 if num_p1 == num:
     print('Победил первый игрок')
