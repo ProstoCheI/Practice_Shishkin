db937babac100dbb68ef390835fa5e3e4131f176
ProstoCheI <shishkinm17@gmail.com>
2024-07-13 20:55:42 +0500

    added clarifying comments

--- Game_num.py
+++ Game_num.py
@@ -1,8 +1,8 @@
 from random import randint
 name_p1 = input('Введите имя первого игрока: ')
 name_p2 = input('Введите имя второго игрока: ')
-num_p1 = int(input('Первый игрок вводит число: '))
-num_p2 = int(input('Второй игрок вводит число: '))
+num_p1 = int(input('Первый игрок вводит число от 1 до 6: '))
+num_p2 = int(input('Второй игрок вводит число от 1 до 6: '))
 num = randint(1, 6)
 if num_p1 == num:
     print('Победил', name_p1)
